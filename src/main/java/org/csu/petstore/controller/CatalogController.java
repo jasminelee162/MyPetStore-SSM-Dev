6233f78b5c505c93d7d27cefd6f2fcbca7840a1a
陳 <144702249+chen125444@users.noreply.github.com>
2025-03-08 16:34:02 +0800

    主界面搭建 catalog对应页面功能等待实现

---
+++ src/main/java/org/csu/petstore/controller/CatalogController.java
@@ -0,0 +1,17 @@
+package org.csu.petstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/catalog")
+public class CatalogController {
+
+
+    @GetMapping("index")
+    public String index(){
+        return "catalog/main";
+    }
+
+}
