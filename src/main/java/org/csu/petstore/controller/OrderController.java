787605ddf9d8ae4da9d489a9420d10ec18ea8e7e
jasmine <1556044893@qq.com>
2025-03-12 23:49:11 +0800

    orderHtml

--- src/main/java/org/csu/petstore/controller/OrderController.java
+++ src/main/java/org/csu/petstore/controller/OrderController.java
@@ -3,10 +3,7 @@ package org.csu.petstore.controller;
 import org.csu.petstore.service.CatalogService;
 import org.csu.petstore.service.OrderService;
 import org.csu.petstore.service.impl.OrderServiceImpl;
-import org.csu.petstore.vo.CategoryVO;
-import org.csu.petstore.vo.ItemVO;
-import org.csu.petstore.vo.OrderVO;
-import org.csu.petstore.vo.ProductVO;
+import org.csu.petstore.vo.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -29,13 +26,13 @@ public class OrderController {
         return "catalog/main";
     }
 
-    //新订单页面控制:还需获得账号查询信息、购物车session信息
-    @GetMapping("newOrderForm")
-    public String newOrderForm(String itemId, Model model) {
-        ItemVO itemVO = orderService.getItem(itemId);
-        model.addAttribute("order", itemVO);
-        return "order/newOrder";
-    }
+//    //新订单页面控制:还需获得账号查询信息、购物车session信息
+//    @GetMapping("newOrderForm")
+//    public String newOrderForm(String itemId, Model model) {
+//        ItemVO itemVO = orderService.getItem(itemId);
+//        model.addAttribute("order", itemVO);
+//        return "order/newOrder";
+//    }
 
     //订单确认页面控制：新订单request获得填写信息的Parameter，并放入session
     @GetMapping("confirmOrder")
@@ -58,5 +55,21 @@ public class OrderController {
         ItemVO itemVO = orderService.getItem(itemId);
         model.addAttribute("item", itemVO);
         return "catalog/item";
+    }
+
+    @GetMapping("newOrder")
+    public String newOrder(String orderId, Model model) {
+        OrderVO orderVO = new OrderVO();
+        orderVO.setOrderId(orderId);
+        model.addAttribute("orderForm", orderVO);
+        return "order/newOrder";
+    }
+
+    @GetMapping("shoppingForm")
+    public String shippingForm(String orderId, Model model) {
+        OrderVO orderVO = new OrderVO();
+        orderVO.setOrderId(orderId);
+        model.addAttribute("orderForm", orderVO);
+        return "order/shoppingForm";
     }
 }
