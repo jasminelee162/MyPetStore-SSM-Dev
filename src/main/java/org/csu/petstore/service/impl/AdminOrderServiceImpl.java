5e4baec9268a6a9eff66263ff83464eacc808551
uuuding <3296416743@qq.com>
2025-03-17 21:53:40 +0800

    AdminOrder 3.17： 1.完成到adminOrder的Mapping 2.初步实现adminOrder页面，完成对全部订单的展示功能 3.adminOrder的css 4.部分实现AdminControllerService

---
+++ src/main/java/org/csu/petstore/service/impl/AdminOrderServiceImpl.java
@@ -0,0 +1,10 @@
+package org.csu.petstore.service.impl;
+
+import org.csu.petstore.service.AdminOrderService;
+
+public class AdminOrderServiceImpl implements AdminOrderService {
+    @Override
+    public boolean getOrders() {
+        return false;
+    }
+}
