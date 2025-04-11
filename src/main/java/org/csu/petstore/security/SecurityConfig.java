88482995ec813767bcde035f9a8a90e6ec0fcbe9
menhera <13672729+menhera_space@user.noreply.gitee.com>
2025-04-11 21:33:11 +0800

    Security接入

--- src/main/java/org/csu/petstore/security/SecurityConfig.java
+++ src/main/java/org/csu/petstore/security/SecurityConfig.java
@@ -1,26 +1,40 @@
 package org.csu.petstore.security;
 
+import org.csu.petstore.security.JwtAuthFilter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import org.springframework.web.filter.OncePerRequestFilter;
 
 @Configuration
 public class SecurityConfig {
 
     @Autowired
-    private JwtAuthFilter jwtAuthFilter;
+    private JwtAuthFilter jwtAuthenticationFilter;
 
     @Bean
-    public FilterRegistrationBean<OncePerRequestFilter> loggingFilter() {
-        FilterRegistrationBean<OncePerRequestFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(jwtAuthFilter);
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                // 禁用 CSRF（根据需要配置）
+                .csrf(csrf -> csrf.disable())
+                .formLogin(formLogin -> formLogin.disable())
 
-        // 拦截所有请求
-        registrationBean.addUrlPatterns("/*");
+                // 配置 URL 权限
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/index", "/tokens", "/", "/catalog/index").permitAll()  // 公开访问
+                                .anyRequest().authenticated()  // 其他请求需要认证
+                )
+                // 添加自定义过滤器
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);  // JWT 认证过滤器
 
-        return registrationBean;
+
+        return http.build();
     }
 }
+
+
